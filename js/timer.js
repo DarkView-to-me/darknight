358372820f36a2d620056b4d52166c1fada42e3a
MrFrustrationMan <34943891+MrFrustrationMan@users.noreply.github.com>
2020-03-09 22:21:27 -0300

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Apr 20, 2020 15:30").getTime();
+var countDownDate = new Date("Jan 1, 2020 15:30").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
